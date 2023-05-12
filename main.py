a49a4420b2f20ceea225c78bc51c29fb19f642d0
Timothy <133363632+3nchant3d@users.noreply.github.com>
2023-05-12 05:00:22 -0700

    Create main.py
    init

---
+++ main.py
@@ -0,0 +1,32 @@
+import base64
+from email.mime.text import MIMEText
+from googleapiclient.discovery import build
+from google.oauth2.credentials import Credentials
+
+# Указываем авторизационные данные и инициализируем сервис Gmail
+creds = Credentials.from_authorized_user_file('credentials.json', ['https://www.googleapis.com/auth/gmail.compose'])
+service = build('gmail', 'v1', credentials=creds)
+
+# Задаем адрес получателя, тему и текст сообщения
+to = 'адрес_получателя@gmail.com'
+subject = 'Новая почта!'
+body = 'Привет, это новое сообщение от меня.'
+
+# Создаем объект MIMEText с текстом сообщения
+message = MIMEText(body)
+
+# Устанавливаем заголовки сообщения
+message['to'] = to
+message['subject'] = subject
+
+# Кодируем сообщение в Base64
+encoded_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
+
+# Создаем объект сообщения для отправки через API
+message = {'raw': encoded_message}
+
+# Отправляем сообщение через API
+send_message = (service.users().messages().send(userId="me", body=message).execute())
+
+# Выводим сообщение об успешной отправке
+print(F'Сообщение успешно отправлено на адрес {to} с ID: {send_message["id"]}.')
